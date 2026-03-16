ce8aed0d1c02417c039f3d8e89793c0c3d202f73
Matth <matth@mtingers.com>
2026-03-15 21:43:42 -0500

    implement object-hash compatibility layer

--- src/compat/object-hash.ts
+++ src/compat/object-hash.ts
@@ -1,5 +1,20 @@
+/**
+ * Drop-in compatibility layer for object-hash v3.0.0.
+ *
+ * Replace:
+ *   const hash = require('object-hash');
+ * With:
+ *   const hash = require('@agentine/signet/compat/object-hash');
+ *
+ * Or for ESM:
+ *   import hash from '@agentine/signet/compat/object-hash';
+ */
 import hash from '../index.js';
 
 export default hash;
-export { hash };
+export const sha1 = hash.sha1;
+export const MD5 = hash.MD5;
+export const keys = hash.keys;
+export const keysMD5 = hash.keysMD5;
+export const writeToStream = hash.writeToStream;
 export type { HashOptions, Algorithm, Encoding } from '../types.js';
